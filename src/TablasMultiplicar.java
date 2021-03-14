1fb01513b1c6a29526859685b81d6a52af473014
Marta <mgomezdeleon@ceinmark.net>
2021-03-14 19:17:03 +0100

    Inicialización

---
+++ src/TablasMultiplicar.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class TablasMultiplicar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Práctica Tablas de Multiplicar
+			saludar();
+			
+		//*****************************************
+		// Crear una función que imprima las 10 tablas de 
+		// multiplicar
+		//*****************************************
+		
+		
+	}
+	public static void saludar() {
+		// TODO Auto-generated method stub
+		System.out.println("Buenos días Ceinmark");
+	}
+}
