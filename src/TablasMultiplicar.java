81b2a22c00f037138751e3d5b37e8639783daa61
Alexander Pichard <39698441+alexanderpichard@users.noreply.github.com>
2021-03-15 09:04:25 +0100

    tabla alexander

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -5,7 +5,7 @@ public class TablasMultiplicar {
 		// TODO Auto-generated method stub
 		// Práctica Tablas de Multiplicar
 			saludar();
-			
+			tablaAlexandr();
 		//*****************************************
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
@@ -17,4 +17,22 @@ public class TablasMultiplicar {
 		// TODO Auto-generated method stub
 		System.out.println("Buenos días Ceinmark");
 	}
-}
+	
+	public static void tablaAlexandr() {
+		System.out.println("----------------");
+			for(int tabla = 1, numero = 1, resultado = tabla * numero; tabla < 11; numero++){
+			
+				if(numero == 11){
+					tabla++;
+					numero = 1;
+					System.out.println("----------------\n");
+				}
+				if(tabla == 11){
+					System.exit(0);
+				}
+				resultado = tabla * numero;
+				System.out.println(tabla + " X " + numero + " = " + resultado);
+			}
+		}
+	}
+
