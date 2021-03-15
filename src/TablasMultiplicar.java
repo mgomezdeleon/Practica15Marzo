5fffaf841ce9910873224cad95d23ba8469801ed
Pablo <Pablo@DESKTOP-DNHR1A7>
2021-03-15 08:56:46 +0100

    Tabla de Pablo

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -10,11 +10,31 @@ public class TablasMultiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
+		tablasPablo();
 		
 	}
 	public static void saludar() {
 		// TODO Auto-generated method stub
 		System.out.println("Buenos días Ceinmark");
+	}
+	
+	public static void tablasPablo() {
+		
+		int tablas=10;
+		
+		for(int i=1;i<tablas;i++) {
+			
+			System.out.println("\n----------\n"
+					+ "Tabla del " + i + 
+					"\n----------\n");
+			
+			for(int j = 0; j<10; j++) {
+				
+				System.out.println(i + " * " + j + " = " + i*j);
+				
+			}
+			
+		}
+		
 	}
 }
