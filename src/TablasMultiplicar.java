b2b12b6be2bc6760064cf233f26104174262c1cc
Jorge <jpardopacheco@ceinmark.net>
2021-03-15 09:00:35 +0100

    Modificacion jorge pardo

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -5,7 +5,7 @@ public class TablasMultiplicar {
 		// TODO Auto-generated method stub
 		// Práctica Tablas de Multiplicar
 			saludar();
-			
+			tablaMultiplicarJorge();
 		//*****************************************
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
@@ -16,5 +16,14 @@ public class TablasMultiplicar {
 	public static void saludar() {
 		// TODO Auto-generated method stub
 		System.out.println("Buenos días Ceinmark");
+	}
+	
+	public static void tablaMultiplicarJorge() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("\n"+"La tabla del: "+ i);
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i+" x "+j+" = "+i*j);
+			}
+		}
 	}
 }
