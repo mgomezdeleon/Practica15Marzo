af8d75b358e58d5d750ae11b9c6c3d14f8bbbeba
Zther <Zther@Zther-Dsk>
2021-03-15 08:57:41 +0100

    Adjunto mi rama

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -10,7 +10,15 @@ public class TablasMultiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
+
+			String linea="";
+			for (int tabla=1; tabla<=10; tabla++) {
+			linea="";
+				for (int multiplo=1; multiplo<=10; multiplo++) {
+					linea+=(multiplo+"x"+tabla+"= "+multiplo*tabla+"  \t");
+				}
+				System.out.println(linea);
+			}
 		
 	}
 	public static void saludar() {
