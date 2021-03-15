85771282063e462481ce9e6eb2cf7eafe726819c
Ceinmark <Ceinmark@DESKTOP-AURVFUI>
2021-03-15 08:52:46 +0100

    Tabla multiplicar Juan David Velasco

--- src/TablasMultiplicar.java
+++ src/TablasMultiplicar.java
@@ -10,8 +10,16 @@ public class TablasMultiplicar {
 		// Crear una función que imprima las 10 tablas de 
 		// multiplicar
 		//*****************************************
-		
-		
+			int num1 = 10;
+			int num2 = 10;
+			String tabla = "";
+			for (int i = 1; i < num1; i++) {
+				for (int j = 1; j < num2; j++) {
+					tabla += j + " * " + i + " = " + (i*j) + "\t ";
+				}
+				tabla += "\n";
+			}
+			System.out.println(tabla);
 	}
 	public static void saludar() {
 		// TODO Auto-generated method stub
